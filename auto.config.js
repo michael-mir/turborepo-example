b1d882f7116f6eccaa36b093f12919a7911ff962
Michael Mironychev <michael.mironychev@gmail.com>
2023-06-30 12:48:28 +0700

    :hot_pepper: upd

--- auto.config.js
+++ auto.config.js
@@ -1,20 +1,5 @@
-/** @type {import('@auto-it/npm').INpmConfig} */
-const npmOptions = {
-  exact: true,
-  canaryScope: '@auto-canary'
-};
-
-/** @type {import('@auto-it/all-contributors').IAllContributorsPluginOptions} */
-const allContributorsOptions = {
-  // types: {
-  //   plugin: '**/plugin/**/*',
-  //   code: ['**/src/**/*', '**/package.json', '**/tsconfig.json']
-  // }
-};
-
 /** @type {import('@auto-it/conventional-commits').ConventionalCommitsOptions} */
 const conventionalCommitsOptions = {
-  preset: 'angular',
   defaultReleaseType: 'patch'
 };
 
@@ -23,12 +8,7 @@ module.exports = () => {
   const autoConfig = {
     baseBranch: 'master',
     author: 'Michael Mironychev <michael.mironychev@gmail.com>',
-    plugins: [
-      'released',
-      ['npm', npmOptions],
-      ['all-contributors', allContributorsOptions],
-      ['conventional-commits', conventionalCommitsOptions]
-    ],
+    plugins: ['released', 'npm', ['conventional-commits', conventionalCommitsOptions]],
     labels: []
   };
 
